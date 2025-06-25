7a75680b35cb15b47e051e8bd32ea15cee5af0cc
Max <100237312+Max13Good@users.noreply.github.com>
2025-06-25 11:52:47 +0300

    Delete student_demo directory

--- student_demo/student_demo/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""WSGI config for student_demo project."""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_demo.settings')
-
-application = get_wsgi_application()
